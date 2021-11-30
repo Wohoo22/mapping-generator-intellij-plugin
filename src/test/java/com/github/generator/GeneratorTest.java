718b8c5b77f92d1a5dc3806e9aa7c6a22967cbb9
quanvda <quandang2102@gmail.com>
2021-11-30 22:35:14 +0700

    test

--- src/test/java/com/github/generator/GeneratorTest.java
+++ src/test/java/com/github/generator/GeneratorTest.java
@@ -20,28 +20,36 @@ public class GeneratorTest {
 
     @Test
     public void runTests() throws Exception {
-        int total = 4;
+        int total = 2;
         for (int i = 1; i <= total; i++) {
             BufferedReader input = FileUtils.readBuffer("/generator/input" + i + ".txt");
 
             String objectToSetQualifiedName = input.readLine();
             String objectToGetVariableName = input.readLine();
             String objectToGetQualifiedName = input.readLine();
-            String mappingType = input.readLine();
             String elementToSetJson = FileUtils.nextJson(input);
             String elementToGetJson = FileUtils.nextJson(input);
             List<ElementNode> elementToSet = new Gson().fromJson(elementToSetJson, NodesWrapper.class).nodes;
             List<ElementNode> elementToGet = new Gson().fromJson(elementToGetJson, NodesWrapper.class).nodes;
 
             JavaToJavaGenerator javaToJavaGenerator = new JavaToJavaGeneratorImpl();
-            String actual = javaToJavaGenerator.generate(JavaToJavaGeneratorPort.GenerateInput.builder()
+            String actualBuilder = javaToJavaGenerator.generate(JavaToJavaGeneratorPort.GenerateInput.builder()
                     .objectToSetQualifiedName(objectToSetQualifiedName)
                     .objectToGetVariableName(objectToGetVariableName)
                     .objectToGetQualifiedName(objectToGetQualifiedName)
-                    .mappingType(JavaToJavaGeneratorPort.GenerateInput.MappingType.valueOf(mappingType))
+                    .mappingType(JavaToJavaGeneratorPort.GenerateInput.MappingType.NORMAL)
                     .elementsToSet(elementToSet)
                     .elementsToGet(elementToGet)
                     .build());
+            String actualNormal = javaToJavaGenerator.generate(JavaToJavaGeneratorPort.GenerateInput.builder()
+                    .objectToSetQualifiedName(objectToSetQualifiedName)
+                    .objectToGetVariableName(objectToGetVariableName)
+                    .objectToGetQualifiedName(objectToGetQualifiedName)
+                    .mappingType(JavaToJavaGeneratorPort.GenerateInput.MappingType.BUILDER)
+                    .elementsToSet(elementToSet)
+                    .elementsToGet(elementToGet)
+                    .build());
+            String actual = actualBuilder + "\n\n############\n\n" + actualNormal;
 
             String expect = FileUtils.read("/generator/output" + i + ".txt");
             try {
